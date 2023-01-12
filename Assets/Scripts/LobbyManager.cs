59e4517a04a0a5eca45705726212e23751cf1fee
bjh3311 <bjh3311@naver.com>
2023-01-11 23:48:57 -1000

    making room

--- Assets/Scripts/LobbyManager.cs
+++ Assets/Scripts/LobbyManager.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
+using Photon.Realtime;
 
-public class LobbyManager : MonoBehaviourPunCallbacks
+public class LobbyManager : MonoBehaviourPunCallbacks//for using PUN2 Network callback
 {
    [Header("---UI Screens---")]
    public GameObject roomUI;
@@ -21,6 +22,42 @@ public class LobbyManager : MonoBehaviourPunCallbacks
     private void Awake() 
     {
         PhotonNetwork.ConnectUsingSettings();
-    }
+    }//Connect to PhotonNetwork based on UsingSetting
+    public override void OnConnectedToMaster()
+    {
+        connectingText.text="Joining Lobby...";
+        PhotonNetwork.JoinLobby(TypedLobby.Default);
+    }// Called when the client is connected to the Master Server and ready for matchmaking and other tasks.
+    public override void OnJoinedLobby()
+    {
+        connectUI.SetActive(false);
+        roomUI.SetActive(true);
+    }// Called on entering a lobby on the Master Server. The actual room-list updates will call OnRoomListUpdate.
     
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel(1);
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        connectUI.SetActive(true);
+        roomUI.SetActive(false);
+        connectingText.text="Disconnected..."+cause.ToString();
+
+    }
+    #region ButtonClicks
+    public void CreateBtn()
+    {
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers=4;
+        PhotonNetwork.CreateRoom(createRoom.text,roomOptions,TypedLobby.Default,null);//take above room options
+    }
+    public void JoinBtn()
+    {
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers=4;
+        PhotonNetwork.JoinOrCreateRoom(joinRoom.text,roomOptions,TypedLobby.Default,null);
+    }
+    #endregion
 }
