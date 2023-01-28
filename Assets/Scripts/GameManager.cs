892a322f53463c452ae995b3a1da44865773ff37
bjh3311 <bjh3311@naver.com>
2023-01-28 01:39:18 -1000

    핑 체크

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 using Photon.Pun;
 
@@ -9,9 +10,16 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     public GameObject player;
     public Transform playerSpawnPosition;
+
+    public Text pingrateTextx;
     void Awake()//First of all, make the player. It makes camera can track the player.
     {
         PhotonNetwork.Instantiate(player.name,playerSpawnPosition.position,playerSpawnPosition.rotation);
+    }
+
+    private void Update() 
+    {
+        pingrateTextx.text=PhotonNetwork.GetPing().ToString();
     }
 
 }
